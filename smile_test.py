8e6e67e8176a089d6cf3b0e5664f35a3c870d84d
Maanav Jajodia <maanavjajodia@gmail.com>
2026-04-20 22:28:23 -0400

    Working Draft

--- smile_test.py
+++ smile_test.py
@@ -1,9 +1,9 @@
 from base_test import BaseTest
-import calculations
+from calculations import face_position
 
 class SmileTest(BaseTest):
 
-    DURATION = 4
+    SMILE_DURATION = 4
     COUNTDOWN_TIME = 3
 
     #This number is not tuned
@@ -19,6 +19,10 @@ class SmileTest(BaseTest):
         self.start_time = None
         self.max_asymmetry = 0.0
         self.last_print = -1
+        self.base_ec_left = None
+        self.base_ec_right = None
+        self.base_mc_left = None
+        self.base_mc_right = None
     
     def get_score(self):
         return self.score
@@ -44,18 +48,56 @@ class SmileTest(BaseTest):
                 print("COUNTDOWN STARTING\n")
 
             case "COUNTDOWN":
-                current_second = int(latest.timestamp - self.countdown_start)
+                time_elapsed = latest.timestamp - self.countdown_start
+                current_second = self.COUNTDOWN_TIME - int(time_elapsed)
+                
                 if current_second != self.last_print and current_second > 0:
-                    print(f"{current_second}")
+                    print(f"{current_second}\n")
+                    self.last_print = current_second
 
-                    if current_second >= self.COUNTDOWN_TIME:
-                        self.status = "SMILING"
-                        self.start_time = latest.timestamp
+                elif time_elapsed >= self.COUNTDOWN_TIME:
+                    print("SMILE\n")
+                    self.status = "SMILING"
+                    self.start_time = latest.timestamp
+                    self.base_mc_left = face_position(landmark, 61)
+                    self.base_mc_right = face_position(landmark, 291)
+                    self.base_ec_left = face_position(landmark, 130)
+                    self.base_ec_right = face_position(landmark, 359)
+                    
                 
             case "SMILING":
-                pass
+                time_elapsed = latest.timestamp - self.start_time
+
+                if time_elapsed <= self.SMILE_DURATION:
+                    current_mc_left = face_position(landmark, 61)
+                    current_mc_right = face_position(landmark, 291)
+                    current_ec_left = face_position(landmark, 130)
+                    current_ec_right = face_position(landmark, 359)
+
+                    base_left_diff = self.base_mc_left[1] - self.base_ec_left[1]
+                    base_right_diff = self.base_mc_right[1] - self.base_ec_right[1]
+                    base_diff = base_left_diff - base_right_diff
+
+                    current_left_diff = current_mc_left[1] - current_ec_left[1]
+                    current_right_diff = current_mc_right[1] - current_ec_right[1]
+                    current_diff = current_left_diff - current_right_diff
+
+                    asymmetry = abs(current_diff - base_diff)
+                    
+                    if asymmetry >= self.max_asymmetry:
+                        self.max_asymmetry = asymmetry
+
+                elif self.max_asymmetry <= self.ASYMMETRY_THRESHOLD:
+                    self.score = "PASS"
+                    self.status = "COMPLETE"
+
+                else:
+                    self.score = "FAIL"
+                    self.status = "COMPLETE"
+                
             case "COMPLETE":
                 pass
+
             case _ :
                 pass
 
